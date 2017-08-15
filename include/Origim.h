b5e989f12a08fecc4ae2d054f02e7859651c2c23
mashiro11 <thiagoaas1990@gmail.com>
2017-08-15 20:47:19 -0300

    ficou faltando

---
+++ include/Origim.h
@@ -0,0 +1,13 @@
+#ifndef ORIGIM_H_INCLUDED
+#define ORIGIM_H_INCLUDED
+
+typedef enum{
+            FACEBOOK = 0,
+            INSTAGRAM,
+            TWITTER,
+            TOPOGRAMAS,
+            NONE
+        }PostSelected;
+
+
+#endif // ORIGIM_H_INCLUDED
